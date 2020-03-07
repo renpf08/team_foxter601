38a81c24c2a6fd65d0641170d1914f7c382c1a86
renpf <tyutrpf@126.com>
2020-03-06 19:53:23 -0800

    add uart.c

--- driver/driver.c
+++ driver/driver.c
@@ -2,9 +2,11 @@
 #include"driver.h"
 
 extern timer_t csr_timer;
+extern uart_t csr_uart;
 
 static driver_t csr_driver = {
 	.timer = &csr_timer,
+	.uart = &csr_uart,
 };
 
 driver_t *get_driver(void)
--- driver/uart/uart.c
+++ driver/uart/uart.c
@@ -1,23 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"../driver.h"
+#include <gatt.h>
+#include <timer.h>
+#include <panic.h>
+#include <mem.h>
+#include <uart.h>
+#include "user_config.h"
+#include "../driver.h"
 
 static int csr_uart_read(void *args)
 {
-
 
 }
 
 static int csr_uart_write(void *args)
 {
 
-
 }
 
 static int csr_uart_init(void *args)
 {
-
+    /* Initialise host interface driver */
+    UartInit(m_uart_recev_irq, 
+             0, /* uart_data_out_fn */
+             uartRxBuffer, UART_BUF_SIZE_BYTES_32,
+             uartTxBuffer, UART_BUF_SIZE_BYTES_64,
+             uart_data_unpacked);
+             
+    /* Configure the UART for high baud rate */
+    UartConfig(HIGH_BAUD_RATE,0);
+        
+    /* Enable UART interface */
+    UartEnable(TRUE);
+    
+    /* Initialise local variables */
+    //req_buffer_level_in_words = 0;
+    
+    /* Request notification from the UART when enough data arrives 
+     * to form a message header. 
+     */
+    UartRead(1, 0);
 	return 0;
 }
 
