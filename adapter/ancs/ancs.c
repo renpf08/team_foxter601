b4aeba2b4b1d0830247dd164b38b7f6df6f8285c
unknown <mlwe_e@qq.com>
2020-04-25 11:05:55 +0800

    change ancs variable to underscore & lowercase

--- adapter/ancs/ancs.c
+++ adapter/ancs/ancs.c
@@ -47,12 +47,12 @@
 #define APP_ID_STRING_LINKEDIN      "com.linkedin.LinkedIn"
 #define APP_ID_STRING_NEWS          "news" //...
 
-typedef struct AppidIndex_T
+typedef struct app_id_index_t
 {
-	const u8 appIndex;
-	const u8 appId[MAX_LENGTH_APPID];
+	const u8 app_index;
+	const u8 app_id[MAX_LENGTH_APPID];
 }APPIDINDEX;
-static const APPIDINDEX appMsgList[] =
+static const APPIDINDEX app_msg_list[] =
 {
     {MESSAGE_POSITION_NONE,         APP_ID_STRING_NONE},
     {MESSAGE_POSITION_LINE,         APP_ID_STRING_LINE},
@@ -79,7 +79,7 @@ static const APPIDINDEX appMsgList[] =
 */
 typedef enum
 {
-    appId = 0x0,
+    appid = 0x0,
     title,
     subtitle,
     message,
@@ -89,18 +89,18 @@ typedef enum
 
 typedef struct
 {
-    u8 recvAttrIdFragment;                       //! receive msg count, total fragment = REQ_ANCS_NOTIF_ATT_ID_TOTAL
-    u8 uuid[4];                                  //! message UUID
-    u8 evtId;                                    //! added, modified, removed, reserved
-    u8 evtFlag;                                  //! silent, important, reserved
-    u8 catId;                                    //! 0other,1incomingCall,2missedCall,3vmail,4social,5schedule,6email,7news,8health,9bussiness,10location,11entertainment,12~255reserved
-    u8 catCnt;                                   //! message count indicated by catId
-    u8 attrIdAppIdData[MAX_LENGTH_ATTTDATA];     //! app name, e.g. QQ->com.tencent.mqq
-    u8 attrIdTitleData[MAX_LENGTH_ATTTDATA];     //! contact name
-    u8 attrIdSubTitleData[MAX_LENGTH_ATTTDATA];  //! sub contact name
-    u8 attrIdMessageData[MAX_LENGTH_ATTTDATA];   //! message content
-    uint16 attrIdMessageSize;                       //! message length
-    u8 attrIdDateData[MAX_LENGTH_ATTTDATA];      //! date and time that the message was received
+    u8 recv_attr_id_fragment;                       //! receive msg count, total fragment = REQ_ANCS_NOTIF_ATT_ID_TOTAL
+    u8 uuid[4];                                     //! message UUID
+    u8 evt_id;                                      //! added, modified, removed, reserved
+    u8 evt_flag;                                    //! silent, important, reserved
+    u8 cat_id;                                      //! 0other,1incomingCall,2missedCall,3vmail,4social,5schedule,6email,7news,8health,9bussiness,10location,11entertainment,12~255reserved
+    u8 cat_cnt;                                     //! message count indicated by catId
+    u8 attr_id_app_id_data[MAX_LENGTH_ATTTDATA];    //! app name, e.g. QQ->com.tencent.mqq
+    u8 attr_id_title_data[MAX_LENGTH_ATTTDATA];     //! contact name
+    u8 attr_id_sub_title_data[MAX_LENGTH_ATTTDATA]; //! sub contact name
+    u8 attr_id_message_data[MAX_LENGTH_ATTTDATA];   //! message content
+    uint16 attr_id_message_size;                    //! message length
+    u8 attr_id_date_data[MAX_LENGTH_ATTTDATA];      //! date and time that the message was received
 } packing_msg_t;
 
 typedef struct
@@ -109,13 +109,13 @@ typedef struct
     u8 appid[MAX_LENGTH_APPID];
 } last_data_map_t;
 
-static last_data_map_t lastData;
-static packing_msg_t pckMsg;
+static last_data_map_t last_data;
+static packing_msg_t pck_msg;
 ancs_msg_t  ancs_msg = {.cmd = 0x07};
 
 
 int ancs_log(const char* file, const char* func, unsigned line, const char* level, const char * sFormat, ...);
-void ancs_business_handle(packing_msg_t* packMsg);
+void ancs_business_handle(packing_msg_t* pack_msg);
 void ancs_data_source_handle(u8 *p_data, u16 size_value, data_source_t *p_data_source);
 void ancs_noti_source_handle(GATT_CHAR_VAL_IND_T *p_ind, noti_t *p_noti_source);
 
@@ -124,27 +124,27 @@ int ancs_log(const char* file, const char* func, unsigned line, const char* leve
     return 0;
 }
 
-void ancs_business_handle(packing_msg_t* packMsg)
+void ancs_business_handle(packing_msg_t* pack_msg)
 {
     u8 i = 0;
 
-    while(appMsgList[i].appId[0] != 0)
+    while(app_msg_list[i].app_id[0] != 0)
     {
-        if(MemCmp(packMsg->attrIdAppIdData, appMsgList[i].appId, sizeof(appMsgList[i].appId)) == 0)
+        if(MemCmp(pack_msg->attr_id_app_id_data, app_msg_list[i].app_id, sizeof(app_msg_list[i].app_id)) == 0)
         {
             break;
         }
         i++;
     }
     
-    MemCopy(lastData.uuid, packMsg->uuid, 4);
-    MemCopy(lastData.appid, appMsgList[i].appId, sizeof(appMsgList[i].appId));
+    MemCopy(last_data.uuid, pack_msg->uuid, 4);
+    MemCopy(last_data.appid, app_msg_list[i].app_id, sizeof(app_msg_list[i].app_id));
     
-    ANCS_LOG_INFO("-> uuid = %02X%02X%02X%02X\r\n", packMsg->uuid[0],packMsg->uuid[1],packMsg->uuid[2],packMsg->uuid[3]);
-    ANCS_LOG_INFO("0> cat id          (notif type) = %d\r\n", packMsg->catId);
-    ANCS_LOG_INFO("1> event id       (notif state) = %d\r\n", packMsg->evtId);
-    //ANCS_LOG_INFO("2> event flag (importance)      = %s\r\n", ancs_notif_event_flag_str[packMsg->evtFlag]);
-    if(appMsgList[i].appId[0] == 0)
+    ANCS_LOG_INFO("-> uuid = %02X%02X%02X%02X\r\n", pack_msg->uuid[0],pack_msg->uuid[1],pack_msg->uuid[2],pack_msg->uuid[3]);
+    ANCS_LOG_INFO("0> cat id          (notif type) = %d\r\n", pack_msg->cat_id);
+    ANCS_LOG_INFO("1> event id       (notif state) = %d\r\n", pack_msg->evt_id);
+    //ANCS_LOG_INFO("2> event flag (importance)      = %s\r\n", ancs_notif_event_flag_str[pack_msg->evt_flag]);
+    if(app_msg_list[i].app_id[0] == 0)
     {
         ANCS_LOG_INFO("2> level           (importance) = <invalid>\r\n");
         ANCS_LOG_INFO("3> attr app id   (message type) = <not found>\r\n");
@@ -153,15 +153,15 @@ void ancs_business_handle(packing_msg_t* packMsg)
     }
     else
     {
-        ANCS_LOG_INFO("2> level           (importance) = %d\r\n", appMsgList[i].appIndex);
-        ANCS_LOG_INFO("3> attr app id   (message type) = %s\r\n", appMsgList[i].appId);
-        ancs_msg.level = appMsgList[i].appIndex;
+        ANCS_LOG_INFO("2> level           (importance) = %d\r\n", app_msg_list[i].app_index);
+        ANCS_LOG_INFO("3> attr app id   (message type) = %s\r\n", app_msg_list[i].app_id);
+        ancs_msg.level = app_msg_list[i].app_index;
         ancs_msg.type = i;
     }
-    ANCS_LOG_INFO("4> cat cnt      (message count) = %d\r\n", packMsg->catCnt); 
+    ANCS_LOG_INFO("4> cat cnt      (message count) = %d\r\n", pack_msg->cat_cnt); 
     
-    ancs_msg.sta = packMsg->evtId;
-    ancs_msg.cnt = packMsg->catCnt;
+    ancs_msg.sta = pack_msg->evt_id;
+    ancs_msg.cnt = pack_msg->cat_cnt;
     SerialSendNotification((u8 *)&ancs_msg, 5); //! send ANCS msg to peer, for test purpose
     //ancs_cb_handler();
 }
@@ -171,44 +171,44 @@ void ancs_data_source_handle(u8 *p_data, u16 size_value, data_source_t *p_data_s
     #if USE_MY_ANCS_DEBUG
     u8 *uuid = p_data_source->uuid;
     ANCS_LOG_DEBUG("++ uuid       = %02X%02X%02X%02X\r\n", uuid[0],uuid[1],uuid[2],uuid[3]);
-    ANCS_LOG_DEBUG("++ attr id    = %d\r\n", p_data_source->attrId);
-    ANCS_LOG_DEBUG("++ attr len   = %d\r\n", p_data_source->attrLen);
-    ANCS_LOG_DEBUG("++ attr data  = %s\r\n", p_data_source->attrData);
+    ANCS_LOG_DEBUG("++ attr id    = %d\r\n", p_data_source->attr_id);
+    ANCS_LOG_DEBUG("++ attr len   = %d\r\n", p_data_source->attr_len);
+    ANCS_LOG_DEBUG("++ attr data  = %s\r\n", p_data_source->attr_data);
     #endif
 
-    if(p_data_source->attrId == appId)
+    if(p_data_source->attr_id == appid)
     {
-        for(i = 0; i < p_data_source->attrLen; i++) pckMsg.attrIdAppIdData[i] = p_data_source->attrData[i];
+        for(i = 0; i < p_data_source->attr_len; i++) pck_msg.attr_id_app_id_data[i] = p_data_source->attr_data[i];
     }
-    else if(p_data_source->attrId == title)
+    else if(p_data_source->attr_id == title)
     {
-        for(i = 0; i < p_data_source->attrLen; i++) pckMsg.attrIdTitleData[i] = p_data_source->attrData[i];
+        for(i = 0; i < p_data_source->attr_len; i++) pck_msg.attr_id_title_data[i] = p_data_source->attr_data[i];
     }
-    else if(p_data_source->attrId == subtitle)
+    else if(p_data_source->attr_id == subtitle)
     {
-        for(i = 0; i < p_data_source->attrLen; i++) pckMsg.attrIdSubTitleData[i] = p_data_source->attrData[i];
+        for(i = 0; i < p_data_source->attr_len; i++) pck_msg.attr_id_sub_title_data[i] = p_data_source->attr_data[i];
     }
-    else if(p_data_source->attrId == message)
+    else if(p_data_source->attr_id == message)
     {
-        for(i = 0; i < p_data_source->attrLen; i++) pckMsg.attrIdMessageData[i] = p_data_source->attrData[i];
+        for(i = 0; i < p_data_source->attr_len; i++) pck_msg.attr_id_message_data[i] = p_data_source->attr_data[i];
     }
-    else if(p_data_source->attrId == messageSize)
+    else if(p_data_source->attr_id == messageSize)
     {
-        for(i = 0; i < p_data_source->attrLen; i++)
+        for(i = 0; i < p_data_source->attr_len; i++)
         {
-            pckMsg.attrIdMessageSize *= 10;
-            pckMsg.attrIdMessageSize += (p_data_source->attrData[i]-'0');
+            pck_msg.attr_id_message_size *= 10;
+            pck_msg.attr_id_message_size += (p_data_source->attr_data[i]-'0');
         }
     }
-    else if(p_data_source->attrId == date)
+    else if(p_data_source->attr_id == date)
     {
-        for(i = 0; i < p_data_source->attrLen; i++) pckMsg.attrIdDateData[i] = p_data_source->attrData[i];
+        for(i = 0; i < p_data_source->attr_len; i++) pck_msg.attr_id_date_data[i] = p_data_source->attr_data[i];
     }
     
-    if(++pckMsg.recvAttrIdFragment >= REQ_ANCS_NOTIF_ATT_ID_TOTAL)
+    if(++pck_msg.recv_attr_id_fragment >= REQ_ANCS_NOTIF_ATT_ID_TOTAL)
     {
-        ancs_business_handle(&pckMsg);
-        MemSet(&pckMsg, 0, sizeof(packing_msg_t));
+        ancs_business_handle(&pck_msg);
+        MemSet(&pck_msg, 0, sizeof(packing_msg_t));
     }
 }
 void ancs_noti_source_handle(GATT_CHAR_VAL_IND_T *p_ind, noti_t *p_noti_source)
@@ -226,38 +226,38 @@ void ancs_noti_source_handle(GATT_CHAR_VAL_IND_T *p_ind, noti_t *p_noti_source)
         return;
     }
     
-    source_t *notiSrc = (source_t*)&p_noti_source->source;
+    source_t *noti_src = (source_t*)&p_noti_source->source;
     /** if noti.source.evtFlag is other value than 1 and 2, then just set it to 3 */
-    notiSrc->evtFlag = ((notiSrc->evtFlag>2)||(notiSrc->evtFlag<1))?3:notiSrc->evtFlag;
+    noti_src->evt_flag = ((noti_src->evt_flag>2)||(noti_src->evt_flag<1))?3:noti_src->evt_flag;
     #if USE_MY_ANCS_DEBUG
-    ANCS_LOG_DEBUG("-- uuid       = %02X%02X%02X%02X\r\n", notiSrc->uuid[0],notiSrc->uuid[1],notiSrc->uuid[2],notiSrc->uuid[3]);
-    ANCS_LOG_DEBUG("-- event id   = %d\r\n", notiSrc->evtId);
-    ANCS_LOG_DEBUG("-- event flag = %d\r\n", notiSrc->evtFlag);
-    ANCS_LOG_DEBUG("-- cat id     = %d\r\n", notiSrc->catId);
-    ANCS_LOG_DEBUG("*- cat cnt    = %d\r\n", notiSrc->catCnt);
+    ANCS_LOG_DEBUG("-- uuid       = %02X%02X%02X%02X\r\n", noti_src->uuid[0],noti_src->uuid[1],noti_src->uuid[2],noti_src->uuid[3]);
+    ANCS_LOG_DEBUG("-- event id   = %d\r\n", noti_src->evt_id);
+    ANCS_LOG_DEBUG("-- event flag = %d\r\n", noti_src->evt_flag);
+    ANCS_LOG_DEBUG("-- cat id     = %d\r\n", noti_src->cat_id);
+    ANCS_LOG_DEBUG("*- cat cnt    = %d\r\n", noti_src->cat_cnt);
     #endif
 
     /** packing stage 1: pack the notif soure */
-    MemSet(&pckMsg, 0, sizeof(packing_msg_t));
+    MemSet(&pck_msg, 0, sizeof(packing_msg_t));
     u8 i =  0;
-    for(i =  0; i < 4; i++) pckMsg.uuid[i] = notiSrc->uuid[i];
-    pckMsg.evtId = notiSrc->evtId;
-    pckMsg.evtFlag = notiSrc->evtFlag;
-    pckMsg.catId = notiSrc->catId;
-    pckMsg.catCnt = notiSrc->catCnt;
+    for(i =  0; i < 4; i++) pck_msg.uuid[i] = noti_src->uuid[i];
+    pck_msg.evt_id = noti_src->evt_id;
+    pck_msg.evt_flag = noti_src->evt_flag;
+    pck_msg.cat_id = noti_src->cat_id;
+    pck_msg.cat_cnt = noti_src->cat_cnt;
     
     /** when incoming call is rejected by receiver, there is no data source to request */
-    if(notiSrc->evtId == ancs_event_id_notif_removed)
+    if(noti_src->evt_id == ancs_event_id_notif_removed)
     {
         /** we just can only handle the newly removed message just after the newly received one 
          *  (means who has the same uuin, but not include missed call)
          */
-        //if((pckMsg.catId == ancs_cat_id_missed_call) || (MemCmp(lastData.uuid, pckMsg.uuid, 4) == 0))
-        if(MemCmp(lastData.uuid, pckMsg.uuid, 4) == 0)
+        //if((pck_msg.cat_id == ancs_cat_id_missed_call) || (MemCmp(last_data.uuid, pck_msg.uuid, 4) == 0))
+        if(MemCmp(last_data.uuid, pck_msg.uuid, 4) == 0)
         {
             LogReport(__FILE__, __func__, __LINE__, M_Ancs_removed_event_occur);
-            MemCopy(pckMsg.attrIdAppIdData, lastData.appid, sizeof(APP_ID_STRING_COMMING_CALL));
-            ancs_business_handle(&pckMsg);
+            MemCopy(pck_msg.attr_id_app_id_data, last_data.appid, sizeof(APP_ID_STRING_COMMING_CALL));
+            ancs_business_handle(&pck_msg);
         }
         else
         {
@@ -268,27 +268,27 @@ void ancs_noti_source_handle(GATT_CHAR_VAL_IND_T *p_ind, noti_t *p_noti_source)
                             lastData.uuid[0],lastData.uuid[1],lastData.uuid[2],lastData.uuid[3],
                             pckMsg.uuid[0],pckMsg.uuid[1],pckMsg.uuid[2],pckMsg.uuid[3]);*/
         }
-        MemSet(&lastData, 0, sizeof(last_data_map_t));
+        MemSet(&last_data, 0, sizeof(last_data_map_t));
     }
     else
     {
         /** we don't need to handle the missed-call event followed by a removed-incomingCall event had handled above */
-        if(pckMsg.catId == ancs_cat_id_missed_call)
+        if(pck_msg.cat_id == ancs_cat_id_missed_call)
         {
             /** do nothing here */
         }
         /** the cat id indicate the known app, so don't need to request to data source */
-        else if((pckMsg.catId == ancs_cat_id_incoming_call)||
-           (pckMsg.catId == ancs_cat_id_email)||
-           (pckMsg.catId == ancs_cat_id_schedule)||
-           (pckMsg.catId == ancs_cat_id_news)) //! did news need to be request data source???
+        else if((pck_msg.cat_id == ancs_cat_id_incoming_call)||
+           (pck_msg.cat_id == ancs_cat_id_email)||
+           (pck_msg.cat_id == ancs_cat_id_schedule)||
+           (pck_msg.cat_id == ancs_cat_id_news)) //! did news need to be request data source???
         {
-			if((pckMsg.catId == ancs_cat_id_incoming_call)) MemCopy(pckMsg.attrIdAppIdData, APP_ID_STRING_COMMING_CALL, sizeof(APP_ID_STRING_COMMING_CALL));
-			if((pckMsg.catId == ancs_cat_id_missed_call)) MemCopy(pckMsg.attrIdAppIdData, APP_ID_STRING_COMMING_CALL, sizeof(APP_ID_STRING_COMMING_CALL));
-			if((pckMsg.catId == ancs_cat_id_email)) MemCopy(pckMsg.attrIdAppIdData, APP_ID_STRING_EMAIL, sizeof(APP_ID_STRING_EMAIL));
-			if((pckMsg.catId == ancs_cat_id_schedule)) MemCopy(pckMsg.attrIdAppIdData, APP_ID_STRING_CALENDAR, sizeof(APP_ID_STRING_CALENDAR));
-			if((pckMsg.catId == ancs_cat_id_news)) MemCopy(pckMsg.attrIdAppIdData, APP_ID_STRING_NEWS, sizeof(APP_ID_STRING_NEWS));
-            ancs_business_handle(&pckMsg);
+			if((pck_msg.cat_id == ancs_cat_id_incoming_call)) MemCopy(pck_msg.attr_id_app_id_data, APP_ID_STRING_COMMING_CALL, sizeof(APP_ID_STRING_COMMING_CALL));
+			if((pck_msg.cat_id == ancs_cat_id_missed_call)) MemCopy(pck_msg.attr_id_app_id_data, APP_ID_STRING_COMMING_CALL, sizeof(APP_ID_STRING_COMMING_CALL));
+			if((pck_msg.cat_id == ancs_cat_id_email)) MemCopy(pck_msg.attr_id_app_id_data, APP_ID_STRING_EMAIL, sizeof(APP_ID_STRING_EMAIL));
+			if((pck_msg.cat_id == ancs_cat_id_schedule)) MemCopy(pck_msg.attr_id_app_id_data, APP_ID_STRING_CALENDAR, sizeof(APP_ID_STRING_CALENDAR));
+			if((pck_msg.cat_id == ancs_cat_id_news)) MemCopy(pck_msg.attr_id_app_id_data, APP_ID_STRING_NEWS, sizeof(APP_ID_STRING_NEWS));
+            ancs_business_handle(&pck_msg);
         }
         
         /** if cat id did not indicate a known app, need to request to data source */
