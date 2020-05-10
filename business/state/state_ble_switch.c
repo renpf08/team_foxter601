15223567fa4d92c0d57286ddec951cfd14fc4957
unknown <mlwe_e@qq.com>
2020-05-10 23:18:55 +0800

    keep swing state

--- business/state/state_ble_switch.c
+++ business/state/state_ble_switch.c
@@ -1,10 +1,11 @@
 #include <debug.h>          /* Simple host interface to the UART driver */
 #include <pio.h>            /* Programmable I/O configuration and control */
-#include "../../common/common.h"
+
+#include "../../common/common.h"
 #include "../../adapter/adapter.h"
 #include "state.h"
 
-#define CLOCK_HAND_SWING_INTERVAL   1000
+#define CLOCK_HAND_SWING_INTERVAL   2000
 bool is_adv_state = FALSE;
 
 static void clock_hand_swing_cb_handler(u16 id)
@@ -13,15 +14,15 @@ static void clock_hand_swing_cb_handler(u16 id)
     
     if(is_adv_state == TRUE) {
         if(init_state == TRUE) {
-            motor_notify_to_position(NOTIFY_DONE);
+            motor_notify_to_position(NOTIFY_DONE-1);
         } else {
-            motor_notify_to_position(NOTIFY_NONE);
+            motor_notify_to_position(NOTIFY_NONE+1);
         }
         init_state = init_state?FALSE:TRUE;
 	    get_driver()->timer->timer_start(CLOCK_HAND_SWING_INTERVAL, clock_hand_swing_cb_handler);
     } else {
         if(init_state == FALSE) {
-            motor_notify_to_position(NOTIFY_NONE);
+            motor_notify_to_position(NOTIFY_NONE+1);
         }
         init_state = TRUE;
     }
@@ -33,13 +34,13 @@ static void ble_is_advertising(bool is_adv)
 }
 s16 state_ble_switch(REPORT_E cb, void *args)
 {
-	STATE_E *state = (STATE_E *)args;
+	//STATE_E *state = (STATE_E *)args;
     
 	u8 string[13] = {'s', 't', 'a', 't', 'e', '_', 'b', 'l', 'e', '_', 's', 'w', 't'};
 	print(string, 13);
 
     ble_is_advertising(ble_switch_get());
 
-	*state = CLOCK;
+	//*state = CLOCK;
 	return 0;
 }
