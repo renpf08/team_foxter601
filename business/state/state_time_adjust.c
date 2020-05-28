cdd33a776ae8d55e14c9574099436f4a5e3acbf7
renpf08 <tyutrpf@126.com>
2020-05-28 20:26:30 +0800

    add time adjust state

--- business/state/state_time_adjust.c
+++ business/state/state_time_adjust.c
@@ -5,10 +5,29 @@
 #include "../../adapter/adapter.h"
 #include "state.h"
 
+
+
+
 s16 state_time_adjust(REPORT_E cb, void *args)
 {
 	print((u8 *)&"time_adjust", 11);
 
+	if(KEY_A_B_LONG_PRESS == cb) {
+		/*minute back to zero position*/
+		state_zero.motor_num = minute_motor;
+	}else if(KEY_M_SHORT_PRESS == cb) {
+		/*motor switcch:minute -> hour ->  date -> battery_week -> minute*/
+		state_zero.motor_num++;
+		if(max_motor == state_zero.motor_num) {
+			state_zero.motor_num = minute_motor;
+		}
+	}else if(KEY_A_SHORT_PRESS == cb) {
+		/*motor run positive half step*/
+	
+	}else if(KEY_B_SHORT_PRESS == cb){
+		/*motor run negtive half step*/
+	
+	}
 
 	return 0;
 }
