445a46c23960894fd40b1a2163788869d3a3e9bd
unknown <mlwe_e@qq.com>
2020-07-05 13:04:26 +0800

    fixed bug of reading hisdata   Words: code=20499, data=3180, free=4721 decimal, img=374KB

--- business/state/state_nvm_access.c
+++ business/state/state_nvm_access.c
@@ -6,8 +6,13 @@
 #include "../../adapter/adapter.h"
 #include "state.h"
 
+static u8 re_in = 0;
 s16 state_access_nvm(REPORT_E cb, void *args)
 {
+    if(re_in == 1) {
+        return 1;
+    }
+    re_in = 1;
     STATE_E *state = (STATE_E *)args;
     s16 res = 0;
     his_data_t data = {0,0,0,0,0,0};
@@ -24,7 +29,7 @@ s16 state_access_nvm(REPORT_E cb, void *args)
         nvm_write_ctrl(&ctrl);
         cmd_set_days(nvm_get_days());
     } else if(cb == READ_HISDATA) {
-        res = nvm_read_data(&data);//nvm_read_history_data((u16*)&data, READ_HISDATA_TOTAL);
+        res = nvm_read_history_data((u16*)&data, READ_HISDATA_TOTAL);//res = nvm_read_data(&data);//
         cmd_set_data(&data);
     } else if(cb == SET_USER_INFO) {
         sport_set(&cmd_get()->user_info);
@@ -40,6 +45,7 @@ s16 state_access_nvm(REPORT_E cb, void *args)
     }
     
 	*state = CLOCK;
+    re_in = 0;
 	return res;
 }
 
