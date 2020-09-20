80f91e53808fcb5b5c7c114eb1f232f3b75cee61
unknown <mlwe_e@qq.com>
2020-09-21 02:11:27 +0800

    optimize compass adjust log

--- business/state/state_compass_adjust.c
+++ business/state/state_compass_adjust.c
@@ -7,19 +7,20 @@
 #include "state.h"
 
 static s16 compass_adj_tid = TIMER_INVALID;
-static u8 adjust_chk[5] = {0,0,0,0,0};
+static u8 adjust_flag = 0;
+//static u8 adjust_chk[9] = {0,0,0,0,0,0,0,0,0};
+//static u8 adjust_chk[9] = {0,0,0,0,0,0,0,0,0};
 
 static void compass_adjust_handler(u16 id)
 {
-    static u8 standby = 0;
     static u8 last_angle = 0;
     u8 angle = angle_get();
-    u16 angle_to_pos = 0;
+    volatile u16 angle_to_pos = 0;
     u8 tmp_angle = 0;
     //u16 adjust_buf[9] = {10,30,50,70,90,110,130,150,170};
-    u16 adjust_buf[5] = {0,40,80,120,160};
+    u16 adjust_buf[8] = {0,25,50,75,100,125,150,175};
     u8 i = 0;
-    u8 ble_log[6] = {CMD_TEST_SEND, BLE_LOG_COMPASS_ADJ, 0, 0, 0, 0};
+    u8 ble_log[7] = {CMD_TEST_SEND, BLE_LOG_COMPASS_ADJ, 0, 0, 0, 0, 0};
 
     if(key_m_ctrl.compass_adj_state == 0) {
         return;
@@ -31,24 +32,14 @@ static void compass_adjust_handler(u16 id)
     	motor_minute_to_position(angle_to_pos);
     	motor_hour_to_position(angle_to_pos);
 
-        if(standby == 1) {
-            standby = 0;
-            MemSet(adjust_chk, 0, sizeof(adjust_chk));
+        if(adjust_flag == 0xFF) { // compass adjust done!
+            adjust_flag = 0;
             vib_stop();
             vib_run(1, 4);
         }
-        for(i = 0; i < sizeof(adjust_chk); i++) {
-            if(adjust_chk[i] != 1) {
-                break;
-            }
-        }
-        if(i >= sizeof(sizeof(adjust_chk))) { // compass adjust done! next do vib to alert
-            standby = 1;
-        }
-        for(i = 0; i < sizeof(adjust_chk); i++) {
-            if(angle == adjust_buf[i]) {
-            if((angle > (adjust_buf[i])) && (angle < (adjust_buf[i]+20)))
-                adjust_chk[i] = 1;
+        for(i = 0; i < sizeof(adjust_buf); i++) {
+            if((angle > (adjust_buf[i])) && (angle < (adjust_buf[i]+5))) {
+                adjust_flag |= (1<<i);
             }
         }
 
@@ -60,7 +51,8 @@ static void compass_adjust_handler(u16 id)
         ble_log[5] = (tmp_angle/10<<4)&0xF0;
         tmp_angle = tmp_angle%10;
         ble_log[5] |= tmp_angle&0x0F;
-        BLE_SEND_LOG(ble_log, 6);
+        ble_log[6] = adjust_flag;
+        BLE_SEND_LOG(ble_log, 7);
     }
     last_angle = angle;
     compass_adj_tid = TIMER_INVALID;
@@ -83,8 +75,7 @@ s16 state_compass_adjust(REPORT_E cb, void *args)
     if(key_m_ctrl.compass_adj_state == 1) {
         hour_pos = motor_sta[minute_motor].dst_pos;
     	motor_hour_to_position(hour_pos);
-        MemSet(adjust_chk, 0, sizeof(adjust_chk));
-        compass_adjust_init();
+        adjust_flag = 0;
         compass_adj_tid = timer_event(1, compass_adjust_handler);
     } else {
     	motor_minute_to_position(clock->minute);
